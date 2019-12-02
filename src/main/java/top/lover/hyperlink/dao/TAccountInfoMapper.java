649882b04f9372502b8dab259eca81cd93103477
qrq <qrqkl123@163.com>
2019-12-02 18:22:10 +0800

    添加菜单接口

---
+++ src/main/java/top/lover/hyperlink/dao/TAccountInfoMapper.java
@@ -0,0 +1,9 @@
+package top.lover.hyperlink.dao;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Mapper;
+import top.lover.hyperlink.entity.TAccountInfo;
+
+@Mapper
+public interface TAccountInfoMapper  extends BaseMapper<TAccountInfo> {
+}
---
+++ src/main/java/top/lover/hyperlink/dao/TMenuInfoMapper.java
@@ -0,0 +1,11 @@
+package top.lover.hyperlink.dao;
+
+import java.util.List;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Mapper;
+import top.lover.hyperlink.entity.TMenuInfo;
+
+@Mapper
+public interface TMenuInfoMapper  extends BaseMapper<TMenuInfo> {
+}
